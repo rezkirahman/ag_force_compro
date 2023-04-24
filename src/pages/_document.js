80ea189b0233d377b559940cd381740457c56d73
Rezki Rahman <rezkirahman1996@gmail.com>
2023-04-24 14:55:31 +0700

    update font & smooth scroll

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
       <Head />
       <body>
         <Main />
