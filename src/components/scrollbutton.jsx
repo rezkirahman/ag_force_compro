54b15a9b323a750c02a3df75a7569f07c27c55fa
Rezki Rahman <rezkirahman1996@gmail.com>
2023-05-02 17:08:28 +0700

    update scroll button

--- src/components/scrollbutton.jsx
+++ src/components/scrollbutton.jsx
@@ -1,10 +1,25 @@
 import { Icon } from "@iconify/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default function ScrollButton(props) {
+export default function ScrollButton() {
+    const [show, setShow] = useState(false)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener("scroll", () => {
+                if (window.pageYOffset > 400) {
+                    setShow(true);
+                } else {
+                    setShow(false);
+                }
+            });
+        }
+    }, [])
+    //if (show) { return null }
+    if (!show) { return null }
     return (
-        <Link href={props.link} className='p-4 bg-primary fixed bottom-6 right-6 z-50 text-white rounded-full shadow-lg'>
+        <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='p-4 bg-primary fixed bottom-6 right-6 z-50 text-white rounded-full shadow-lg'>
             <Icon icon='akar-icons:align-to-top' className='lg:text-2xl' />
-        </Link>
+        </button>
     );
 }
