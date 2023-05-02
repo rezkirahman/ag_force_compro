efa247640e8b42e0826ecaea2a1a3c1b6f272466
Rezki Rahman <rezkirahman1996@gmail.com>
2023-05-02 11:40:36 +0700

    button footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,6 @@ export default function name(props) {
                     <div className="bg-white">
                         <Image src={'/logo.png'} width={400} height={100} alt="" className="w-[74px] h-auto lg:w-[140px]" />
                     </div>
-                    <button>
                         <Link
                             href={props.isAndroid ? 'https://play.google.com/store/apps/details?id=com.ag.agforce&hl=id' : (props.isIOS ? 'https://apps.apple.com/us/app/ag-force/id6444120625bit.ly/agforceios' : '#kontak')}
                             className="bg-white rounded-full py-4 px-6 ring-2 ring-white text-primary font-semibold hover:bg-primary hover:text-white transition-all duration-300"
@@ -18,7 +17,6 @@ export default function name(props) {
                         >
                             Unduh Sekarang
                         </Link>
-                    </button>
                 </div>
                 <div className="space-y-5 lg:space-y-0 text-white mt-8 lg:flex lg:items-center lg:justify-between">
                     <Link
